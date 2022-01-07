4767ed12f3868dd124ac57dff2688c92fb4a3f8f
Gabriel Ve Vance <42804193+gvevance@users.noreply.github.com>
2022-01-07 11:25:00 +0530

    Search idea figured out.

--- Version3/automate_nmos_v3.py
+++ Version3/automate_nmos_v3.py
@@ -4,12 +4,17 @@
 # efficient DSA search algorithm and some efficient file handling.
 
 from subprocess import call
+import numpy as np
+import matplotlib.pyplot as plt
 
 vmin_tolerance = 0.05  #! cannot give voltage as 0. So min tolerance
 
 cir_filename = 'temp_nmos_v3.cir'
-value_file = 'values_nmos_v2.txt'
+value_file = 'values_nmos_v3.txt'
 model_file = '130nm_bulk.pm'
+constraints_file = 'constraints.txt'
+
+width = '100' # in um
 
 def define_constraints():
     ''' Define the constraints to search for after user-input. Writes to a file for the program to read from. 
@@ -19,24 +24,35 @@ def define_constraints():
     print("\nWhat parameters do you have constraints on ? Enter appropriate code (\"0\" when done)...\n")
     print("\"gm_wid\" - gm/W \n\"gds_wid\" - gds/W \n\"gain\" - gm/gds \n\"ft\" - ft \n")
     
+    # clear already existing lines in constraints.txt
+    c_file = open(constraints_file,"r+")
+    c_file.truncate(0)
+    c_file.close()
+
+    c_file = open(constraints_file,'a') 
     ret = "start"
     while(ret != '0'):
         ret = input("Enter option : ")
         
         if (ret == "gm_wid") :
             gm_wid_min , gm_wid_max = input("Enter gm/W range : ").split()
+            c_file.write("gm_wid "+gm_wid_min+" "+gm_wid_max+"\n")
 
         elif (ret == "gds_wid"):
             gds_wid_min , gds_wid_max = input("Enter gds/W range : ").split()
+            c_file.write("gds_wid "+gds_wid_min+" "+gds_wid_max+"\n")
 
         elif (ret == "gain"):
             gain_min , gain_max = input("Enter gain range : ").split()
+            c_file.write("gain "+gain_min+" "+gain_max+"\n")
 
         elif (ret == "ft"):
             ft_min , ft_max = input("Enter ft range : ").split()
+            c_file.write("ft "+ft_min+" "+ft_max+"\n")
 
         elif (ret == "0"):
-            pass
+            print()
+            c_file.close()
 
         else :
             print("Enter valid option. \"0\" if you're done.")
@@ -47,21 +63,115 @@ def define_constraints():
     
 def generate_contents(len,vgs_min,vgs_max):
     
-    if vgs_min == 0 :
-        vgs_min += vmin_tolerance
+    if vgs_min == '0' :
+        vgs_min = str(vmin_tolerance)
     
-    return '''Dummy script
-    .control
-    exit
-    .endc
-    .end'''
+    contents = f'''NMOS characterisation
+
+*************************************
+* Include model file 
+*************************************
+.include {model_file}
+
+*************************************
+* Defining arameters 
+*************************************
+.param len = {len}u
+.param width = {width}u 
+.param lmin = 0.13u
+
+*************************************
+* Circuit definition
+*************************************
+vgs 1 0 dc 0.5
+M1 1 1 0 0 nmos l={{len}} w={{width}} as={{2*lmin*width}} ad={{2*lmin*width}} ps={{4*lmin+2*width}} pd={{4*lmin+2*width}}
+
+*************************************
+* Control section
+*************************************
+
+.control 
+
+save @M1[id], @M1[vdsat], @M1[vth], @M1[cgs], @M1[cgg],
++ @M1[gm], @M1[gds], @M1[gmbs], @M1[vsat]
+
+* DC sweep
+dc vgs {vgs_min} {vgs_max} 0.01
+
+* Run the sim
+run
+
+set filetype=ascii
+set wr_singlescale
+set wr_vecnames
+
+wrdata {value_file} @M1[id], @M1[vdsat], @M1[cgs], @M1[cgg] 
++ @M1[gm], @M1[gds], @M1[gmbs], @M1[vth]
+
+* Plot commands
+* plot @M1[id]
+* plot @M1[gm]
+* plot @M1[gmbs]
+* plot @M1[gds]
+* plot @M1[cgs]
+* plot @M1[cgg]
+* plot @M1[vdsat]
+* plot @M1[vsat]
+
+exit
+.endc
+
+*************************************
+* End of file
+*************************************
+.end
+'''
+
+    return contents
 
 def write_cir(contents):
     with open(cir_filename,'w') as file :
         file.write(contents)
 
-def op_search():
-    pass
+def extract_params():
+    
+    width_c = float(width) * 1e-6
+
+    with open(value_file) as txtfile :
+        temp = np.genfromtxt(txtfile, dtype=float)
+
+    vgs    = temp[1:,0]
+    id     = temp[1:,1]
+    vdsat  = temp[1:,2]
+    cgs    = -temp[1:,3]
+    cgg    = temp[1:,4]
+    gm     = temp[1:,5]
+    gds    = temp[1:,6]
+    gmbs   = temp[1:,7]
+    vth    = temp[1:,8]
+
+    # custom definitions
+
+    gm_by_id   = gm/id 
+    gain       = gm/gds
+    ft         = gm/cgg
+    gm_by_gmbs = gm/gmbs
+
+    # /width quantities
+
+    id_wid     = id/width_c
+    gm_wid     = gm/width_c
+    gds_wid    = gds/width_c
+    cgg_wid    = cgg/width_c
+    cgs_wid    = cgs/width_c
+    gmbs_wid   = gmbs/width_c
+
+    return [vgs,gm_by_id,id_wid,vdsat,cgs_wid,cgg_wid,gm_wid,gds_wid,vth,gain,ft,gmbs_wid,gm_by_gmbs]
+
+def op_search(params):
+    
+    vgs,gm_by_id,id_wid,vdsat,cgs_wid,cgg_wid,gm_wid,gds_wid,vth,gain,ft,gmbs_wid,gm_by_gmbs = params
+    print(vgs[(gm_wid <50) & (gm_wid > 30)])
 
 def main():
 
@@ -74,7 +184,8 @@ def main():
     write_cir(contents)
     call(['ngspice',cir_filename])
 
-    op_search()  # does not need any input. Uses the sim results and constraits file 
+    params = extract_params()
+    op_search(params)  # does not need any input. Uses the sim results and constraits file 
 
 
 if __name__=="__main__":
