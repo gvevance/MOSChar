f596abfd9d44b55857f767239ca050008251f49f
Gabriel Ve Vance <42804193+gvevance@users.noreply.github.com>
2022-01-15 15:53:14 +0530

    Plot in logscale instead of np.log10

--- Version2/automate_nmos_v2.py
+++ Version2/automate_nmos_v2.py
@@ -17,65 +17,65 @@ def generate_contents(length,width):
     
     contents = f'''NMOS characterisation
 
-    *************************************
-    * Include model file 
-    *************************************
-    .include {model_file}
+*************************************
+* Include model file 
+*************************************
+.include {model_file}
 
-    *************************************
-    * Defining arameters 
-    *************************************
-    .param len = {length}u
-    .param width = {width}u 
-    .param lmin = 0.13u
+*************************************
+* Defining arameters 
+*************************************
+.param len = {length}u
+.param width = {width}u 
+.param lmin = 0.13u
 
-    *************************************
-    * Circuit definition
-    *************************************
-    vgs 1 0 dc 0.5
-    M1 1 1 0 0 nmos l={{len}} w={{width}} as={{2*lmin*width}} ad={{2*lmin*width}} ps={{4*lmin+2*width}} pd={{4*lmin+2*width}}
+*************************************
+* Circuit definition
+*************************************
+vgs 1 0 dc 0.5
+M1 1 1 0 0 nmos l={{len}} w={{width}} as={{2*lmin*width}} ad={{2*lmin*width}} ps={{4*lmin+2*width}} pd={{4*lmin+2*width}}
 
-    *************************************
-    * Control section
-    *************************************
+*************************************
+* Control section
+*************************************
 
-    .control 
+.control 
 
-    save @M1[id], @M1[vdsat], @M1[vth], @M1[cgs], @M1[cgg],
-    + @M1[gm], @M1[gds], @M1[gmbs], @M1[vsat]
+save @M1[id], @M1[vdsat], @M1[vth], @M1[cgs], @M1[cgg],
++ @M1[gm], @M1[gds], @M1[gmbs], @M1[vsat]
 
-    * DC sweep
-    dc vgs 0.05 1.3 0.01
+* DC sweep
+dc vgs 0.05 1.3 0.01
 
-    * Run the sim
-    run
+* Run the sim
+run
 
-    set filetype=ascii
-    set wr_singlescale
-    set wr_vecnames
+set filetype=ascii
+set wr_singlescale
+set wr_vecnames
 
-    wrdata {value_file} @M1[id], @M1[vdsat], @M1[cgs], @M1[cgg] 
-    + @M1[gm], @M1[gds], @M1[gmbs], @M1[vth]
+wrdata {value_file} @M1[id], @M1[vdsat], @M1[cgs], @M1[cgg] 
++ @M1[gm], @M1[gds], @M1[gmbs], @M1[vth]
 
-    * Plot commands
-    * plot @M1[id]
-    * plot @M1[gm]
-    * plot @M1[gmbs]
-    * plot @M1[gds]
-    * plot @M1[cgs]
-    * plot @M1[cgg]
-    * plot @M1[vdsat]
-    * plot @M1[vsat]
+* Plot commands
+* plot @M1[id]
+* plot @M1[gm]
+* plot @M1[gmbs]
+* plot @M1[gds]
+* plot @M1[cgs]
+* plot @M1[cgg]
+* plot @M1[vdsat]
+* plot @M1[vsat]
 
 
-    exit
-    .endc
+exit
+.endc
 
-    *************************************
-    * End of file
-    *************************************
-    .end
-    '''
+*************************************
+* End of file
+*************************************
+.end
+'''
 
     return contents
 
@@ -131,66 +131,66 @@ def plot_figures(sim_values,length,plot_list):
         plt.grid(True)
         plt.legend()
 
-    if "id/W" in plot_list:
+    if "id/w" in plot_list:
         plt.figure(2) # log10(id/W) vs gm/Id
-        plt.plot( gm_by_id , np.log10(id_wid) , label='Len = '+length+'u' )
-        plt.ylabel("log10(Id/W)")
+        plt.semilogy( gm_by_id , id_wid , label='Len = '+length+'u' )
+        plt.ylabel("Id/W")
         plt.xlabel("gm/Id")
-        plt.title("Plot of log10(Id/W) vs gm/Id")
+        plt.title("Plot of Id/W vs gm/Id")
         plt.grid(True)
         plt.legend()
 
-    if "gm/W" in plot_list:
+    if "gm/w" in plot_list:
         plt.figure(3) # log10(gm/W) vs gm/Id
-        plt.plot( gm_by_id , np.log10(gm_wid) , label='Len = '+length+'u' )
-        plt.ylabel("log10(gm/W)")
+        plt.semilogy( gm_by_id , gm_wid , label='Len = '+length+'u' )
+        plt.ylabel("gm/W")
         plt.xlabel("gm/Id")
-        plt.title("Plot of log10(gm/W) vs gm/Id")
+        plt.title("Plot of gm/W vs gm/Id")
         plt.grid(True)
         plt.legend()
 
-    if "gds/W" in plot_list:
+    if "gds/w" in plot_list:
         plt.figure(4) # log10(gds/W) vs gm/Id
-        plt.plot( gm_by_id , np.log10(gds_wid) , label='Len = '+length+'u' )
-        plt.ylabel("log10(gds/W)")
+        plt.semilogy( gm_by_id , gds_wid , label='Len = '+length+'u' )
+        plt.ylabel("gds/W")
         plt.xlabel("gm/Id")
-        plt.title("Plot of log10(gds/W) vs gm/Id")
+        plt.title("Plot of gds/W vs gm/Id")
         plt.grid(True)
         plt.legend()
 
     if "gain" in plot_list:
         plt.figure(5) # log10(gain/W) vs gm/Id
-        plt.plot( gm_by_id , np.log10(gain) , label='Len = '+length+'u' )
-        plt.ylabel("log10(gain)")
+        plt.semilogy( gm_by_id , gain , label='Len = '+length+'u' )
+        plt.ylabel("gain")
         plt.xlabel("gm/Id")
-        plt.title("Plot of log10(gain) vs gm/Id")
+        plt.title("Plot of gain vs gm/Id")
         plt.grid(True)
         plt.legend()
 
-    if "cgg/W" in plot_list:
+    if "cgg/w" in plot_list:
         plt.figure(6) # log10(cgg/W) vs gm/Id
-        plt.plot( gm_by_id , np.log10(cgg_wid) , label='Len = '+length+'u' )
-        plt.ylabel("log10(cgg/W)")
+        plt.semilogy( gm_by_id , cgg_wid , label='Len = '+length+'u' )
+        plt.ylabel("cgg/W")
         plt.xlabel("gm/Id")
-        plt.title("Plot of log10(cgg/W) vs gm/Id")
+        plt.title("Plot of cgg/W vs gm/Id")
         plt.grid(True)
         plt.legend()
 
-    if "cgs/W" in plot_list:
+    if "cgs/w" in plot_list:
         plt.figure(7) # log10(cgs/W) vs gm/Id
-        plt.plot( gm_by_id , np.log10(cgs_wid) , label='Len = '+length+'u' )
-        plt.ylabel("log10(cgs/W)")
+        plt.semilogy( gm_by_id , cgs_wid , label='Len = '+length+'u' )
+        plt.ylabel("cgs/W")
         plt.xlabel("gm/Id")
-        plt.title("Plot of log10(cgs/W) vs gm/Id")
+        plt.title("Plot of cgs/W vs gm/Id")
         plt.grid(True)
         plt.legend()
 
     if "ft" in plot_list:
         plt.figure(8) # log10(ft) vs gm/Id
-        plt.plot( gm_by_id , np.log10(ft) , label='Len = '+length+'u' )
-        plt.ylabel("log10(ft)")
+        plt.semilogy( gm_by_id , ft , label='Len = '+length+'u' )
+        plt.ylabel("ft")
         plt.xlabel("gm/Id")
-        plt.title("Plot of log10(ft) vs gm/Id")
+        plt.title("Plot of ft vs gm/Id")
         plt.grid(True)
         plt.legend()
 
@@ -212,21 +212,21 @@ def plot_figures(sim_values,length,plot_list):
         plt.grid(True)
         plt.legend()
 
-    if "gmbs/W" in plot_list:
+    if "gmbs/w" in plot_list:
         plt.figure(11) # log10(gmbs/W) vs gm/Id
-        plt.plot( gm_by_id , np.log10(gmbs_wid) , label='Len = '+length+'u' )
-        plt.ylabel("log10(gmbs/W)")
+        plt.semilogy( gm_by_id , gmbs_wid , label='Len = '+length+'u' )
+        plt.ylabel("gmbs/W")
         plt.xlabel("gm/Id")
-        plt.title("Plot of log10(gmbs/W) vs gm/Id")
+        plt.title("Plot of gmbs/W vs gm/Id")
         plt.grid(True)
         plt.legend()
 
     if "gm/gmbs" in plot_list:
         plt.figure(12) # log10(gm/gmbs) vs gm/Id
-        plt.plot( gm_by_id , np.log10(gm_by_gmbs) , label='Len = '+length+'u' )
-        plt.ylabel("log10(gm/gmbs)")
+        plt.plot( gm_by_id , gm_by_gmbs , label='Len = '+length+'u' )
+        plt.ylabel("gm/gmbs")
         plt.xlabel("gm/Id")
-        plt.title("Plot of log10(gm/gmbs) vs gm/Id")
+        plt.title("Plot of gm/gmbs vs gm/Id")
         plt.grid(True)
         plt.legend()
 
@@ -237,8 +237,8 @@ def main():
     width = str(input("Enter width (in um) : "))
     len_list = str(input("Enter lengths (in um) (space-separated) : ")).split()
     
-    print("\nCodes : gm/id, vdsat, gm/W, gds/W, id/W, cgs/W, cgg/W, vth, gain, ft, gmbs/W, gm/gmbs \n")
-    plot_superlist = ["gm/id", "vdsat", "gm/W", "gds/W", "id/W", "cgs/W", "cgg/W", "vth", "gain", "ft", "gmbs/W", "gm/gmbs"]
+    print("\nCodes : gm/id, vdsat, gm/w, gds/w, id/W, cgs/w, cgg/w, vth, gain, ft, gmbs/w, gm/gmbs \n")
+    plot_superlist = ["gm/id", "vdsat", "gm/w", "gds/w", "id/w", "cgs/w", "cgg/w", "vth", "gain", "ft", "gmbs/w", "gm/gmbs"]
     plot_list = input("Enter quantities you want to plot using appropriate codes : ").split()
 
     while ((set(plot_list).issubset(set(plot_superlist)) == False ) or (len(plot_list) == 0)):
